4f451ab702a5d0c9e60bf5749013e6f0ae440db5
Meirion Williams <meirion.williams.1@city.ac.uk>
2011-01-24 09:09:51 +0000

    chap 7.2 cont

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,13 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
   
+  #return a user object matching supplier email and password
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
   private
    
     def encrypt_password
