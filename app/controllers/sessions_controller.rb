cb6a85196af39f19366386f3609f0387f6a99966
Meirion Williams <meirion.williams.1@city.ac.uk>
2011-01-31 17:52:14 +0000

    ch9 pt 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,18 @@ class SessionsController < ApplicationController
   
   # create handles form submission and makes new session
   def create
-    render 'new'  # show blank forms
+    #render 'new'  # show blank forms
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      #create error message and re-render signin form
+      flash.now[:error] = "Sorry your username and/or password were not correct."
+      @title = "Sign in"
+      render = 'new'
+    else
+      #sign the user in and redirect to the user's show page
+    end
+              
   end
   
   # accessed via HTTP DELETE, deletes a session
